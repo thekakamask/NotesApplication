611ad4a4ea1aadb01de3fa978eca840e3ceeda85
Antoine de Chaunac <adechaunac@hotmail.fr>
2023-11-21 12:15:01 +0100

    ajout de la fonctionnalité des smileys permettant ajouter les smileys avec les 3 boutons au texte

--- app/src/main/java/com/example/notesapplication/NotePadFragment.java
+++ app/src/main/java/com/example/notesapplication/NotePadFragment.java
@@ -2,6 +2,7 @@ package com.example.notesapplication;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,7 +11,11 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -78,12 +83,43 @@ public class NotePadFragment extends Fragment {
     }
 
     private void SmileyWinkToText() {
+
+        Drawable smileyDrawable = ContextCompat.getDrawable(getContext(), R.drawable.smiley_wink);
+        if (smileyDrawable != null) {
+            smileyDrawable.setBounds(0, 0, smileyDrawable.getIntrinsicWidth(), smileyDrawable.getIntrinsicHeight());
+            SpannableString spannableString = new SpannableString(" ");
+            ImageSpan imageSpan = new ImageSpan(smileyDrawable, ImageSpan.ALIGN_BOTTOM);
+            spannableString.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            int start = binding.editTextInput.getSelectionStart();
+            binding.editTextInput.getText().insert(start, spannableString);
+        }
     }
 
     private void SmileySmileToText() {
+        Drawable smileyDrawable = ContextCompat.getDrawable(getContext(), R.drawable.smiley_smile);
+        if (smileyDrawable != null) {
+            smileyDrawable.setBounds(0, 0, smileyDrawable.getIntrinsicWidth(), smileyDrawable.getIntrinsicHeight());
+            SpannableString spannableString = new SpannableString(" ");
+            ImageSpan imageSpan = new ImageSpan(smileyDrawable, ImageSpan.ALIGN_BOTTOM);
+            spannableString.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            int start = binding.editTextInput.getSelectionStart();
+            binding.editTextInput.getText().insert(start, spannableString);
+        }
     }
 
     private void SmileyBigSmileToText() {
+        Drawable smileyDrawable = ContextCompat.getDrawable(getContext(), R.drawable.smiley_big_smile);
+        if (smileyDrawable != null) {
+            smileyDrawable.setBounds(0, 0, smileyDrawable.getIntrinsicWidth(), smileyDrawable.getIntrinsicHeight());
+            SpannableString spannableString = new SpannableString(" ");
+            ImageSpan imageSpan = new ImageSpan(smileyDrawable, ImageSpan.ALIGN_BOTTOM);
+            spannableString.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            int start = binding.editTextInput.getSelectionStart();
+            binding.editTextInput.getText().insert(start, spannableString);
+        }
     }
 
     private void HideMenu() {
@@ -105,7 +141,9 @@ public class NotePadFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                binding.textPreview.setText(s.toString());
+                binding.textPreview.setText(binding.editTextInput.getText());
+
+
             }
 
             @Override
